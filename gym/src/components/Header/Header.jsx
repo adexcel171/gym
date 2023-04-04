95cf81b754c9784f5bd8d238ff28450231ab9688
Godswii Excel Okenyi <98773440+adexcel171@users.noreply.github.com>
2023-04-04 03:10:58 -0700

    first commit

---
+++ gym/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+import logo from '../../assets/logo.png'
+
+const Header = () => {
+  return (
+    <div className='header'>
+           <img src={logo} alt='img' className='logo'></img>
+           <ul className='header-menu'>
+                <li>Home</li>
+                <li>Programs</li>
+                <li>Why us</li>
+                <li>Plans</li>
+                <li>Testimonials</li>
+           </ul>
+    </div>
+  )
+}
+
+export default Header
---
+++ gym/src/components/Hero/Hero.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import Header from "../Header/Header.jsx";
+import "./Hero.css";
+import hero_image from '../../assets/hero_image.png';
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="left-h">
+        <Header />
+
+        {/* the best ad */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>The best fitness club in the town</span>
+        </div>
+
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+
+          <div>
+            <span>Ideal body</span>
+          </div>
+
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
+        </div>
+
+        {/* figures */}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        {/* hero buttons */}
+        <div className="hero-buttons">
+           <button className="btn">Get Started</button>
+           <button className="btn">Learn More</button>
+        </div>
+
+      </div>
+
+
+      {/* Right side */}
+      <div className="right-h">
+          <button className="btn">
+              Join Now
+          </button>
+
+          <div className="heart-rate">
+            <img src={Heart} alt="heart" />
+            <span>Heart Rate</span>
+            <span>116 bpm</span>
+          </div>
+      </div>
+
+      {/* hero image */}
+      <img src={hero_image} alt="Body" className="hero-image"/>
+      <img src={hero_image_back} alt="Body-Asset" className="hero-image-back"/>
+      
+      {/* calories */}
+      <div className="calories">
+        <img src={Calories} alt="Calories" />
+        <div>
+          <span>Calories Burned</span>
+          <span>220 kcal</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
